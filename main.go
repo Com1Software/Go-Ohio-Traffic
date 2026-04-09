cb006100ff86a6fc69579fff932e284f71150b36
Lenovo 15 <information@com1software.com>
2026-04-09 10:58:38 -0400

    copilot update

--- main.go
+++ main.go
@@ -1,38 +1,63 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "net/http"
-    "os"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
-type Incident struct {
-    ID          string `json:"id"`
-    Type        string `json:"type"`
-    Description string `json:"description"`
-    StartTime   string `json:"startTime"`
-    Road        string `json:"road"`
-    Direction   string `json:"direction"`
+type ArcGISResponse struct {
+	Features []struct {
+		Attributes IncidentAttributes `json:"attributes"`
+	} `json:"features"`
+}
+
+type IncidentAttributes struct {
+	ID          int     `json:"OBJECTID"`
+	Road        string  `json:"ROADWAY"`
+	Direction   string  `json:"DIRECTION"`
+	Description string  `json:"DESCRIPTION"`
+	County      string  `json:"COUNTY"`
+	Latitude    float64 `json:"LATITUDE"`
+	Longitude   float64 `json:"LONGITUDE"`
+	Severity    string  `json:"SEVERITY"`
 }
 
 func main() {
-    apiKey := os.Getenv("OHGO_API_KEY")
-    url := "https://api.ohgo.com/v1/incidents?key=" + apiKey
+	url := "https://gis.dot.state.oh.us/arcgis/rest/services/Transportation/Traffic_Incidents/FeatureServer/0/query?where=1%3D1&outFields=*&f=json"
 
-    resp, err := http.Get(url)
-    if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Request error:", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
 
-    var incidents []Incident
-    if err := json.NewDecoder(resp.Body).Decode(&incidents); err != nil {
-        panic(err)
-    }
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Read error:", err)
+		os.Exit(1)
+	}
 
-    for _, inc := range incidents {
-        fmt.Printf("%s: %s on %s %s\n",
-            inc.ID, inc.Description, inc.Road, inc.Direction)
-    }
+	fmt.Println("RAW RESPONSE:")
+	fmt.Println(string(body))
+
+	// Prevent JSON decode on HTML or empty body
+	if len(body) == 0 || body[0] == '<' {
+		fmt.Println("\nERROR: Server returned HTML or empty response, not JSON.")
+		os.Exit(1)
+	}
+
+	var data ArcGISResponse
+	if err := json.Unmarshal(body, &data); err != nil {
+		fmt.Println("JSON decode error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("\nParsed incidents:")
+	for _, f := range data.Features {
+		fmt.Printf("%+v\n", f.Attributes)
+	}
 }
